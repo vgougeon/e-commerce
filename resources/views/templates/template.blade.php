ca6d78469ea247c9729fe1201fd9106b18d67dd1
Vincent GOUGEON <gougeonvincent31@outlook.fr>
2020-04-02 09:49:43 +0200

    Base

---
+++ resources/views/templates/template.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>playzone</title>
+</head>
+<body>
+    
+</body>
+</html>
