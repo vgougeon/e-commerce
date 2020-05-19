02cc0aa069c94289f3339f3be8628e150a2360a5
Thomas Alzonne <thomas.alzonne@ynov.com>
2020-05-19 11:55:28 +0200

    seeders

--- database/seeds/GameSeeder.php
+++ database/seeds/GameSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Game;
 class GameSeeder extends Seeder
 {
     /**
@@ -11,6 +11,23 @@ class GameSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $game1 = new Game;
+        $game1->name = "League Of Legends";
+        $game1->price = 150.99;
+        $game1->description = "jeu du démon";
+
+        $game2 = new Game;
+        $game2->name = "Animal Crossing";
+        $game2->price = 49.99;
+        $game2->description = "jeu de détente";
+
+        $game3 = new Game;
+        $game3->name = "Nintendogs";
+        $game3->price = 1400.99;
+        $game3->description = "jeu de chien";
+
+        $game1->save();
+        $game2->save();
+        $game3->save();
     }
 }
