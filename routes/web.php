47b9e986bbb9ccda40a96e2534e98ddb65b53bfe
thomas <thomas.alzonne@ynov.com>
2020-05-27 12:24:53 +0200

    sending mails with pdf

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Mail\MailtrapExample;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', 'MainController@home')->name('/');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/game/{id}', 'GameController@show')->name('game');
 Route::get('/editprofile/{id}', 'MainController@edit');
-Route::get('/editprofile/update', 'MainController@update')->name('main.update');
+Route::post('/editprofile/update', 'MainController@update')->name('main.update');
 Auth::routes();
 Route::middleware(['admin'])->group(function () {
     Route::get('/admin', 'AdminController@panel')->name('admin');
@@ -14,4 +16,11 @@ Route::middleware(['admin'])->group(function () {
         'index' => 'admin.members'
     ]);
     Route::resource('admin/games', 'GameController');
+});
+Route::get('/send-mail', function () {
+
+    Mail::to('newuser@example.com')->send(new MailtrapExample());
+
+    return 'A message has been sent to Mailtrap!';
+
 });
